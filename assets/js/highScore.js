aa1b50da391f21857d10c4787992f7791db2cd59
Lu Hao <haolu1125@gmail.com>
2020-12-07 18:03:50 -0500

    add logic

--- assets/js/highScore.js
+++ assets/js/highScore.js
@@ -1,14 +1,40 @@
 
 //localstore get past stored scores
-function showHighScore(answer) {
-    var recordAll = JSON.parse(localStorage.getItem("historyRecord"));
-    console.log(recordAll);
-    start.classList.add("hide");
-    highScoreBtn.classList.add("hide");
-    highscoreContainer.classList.remove("hide");
-    highScoreInitials.innerHTML = recordAll.name;
-    highestScore.innerHTML = recordAll.score;
-    endSection.classList.add("hide");
-    start.classList.remove("hide");
-    start.innerHTML = "Restart";
+function showHighScore() {
+    // get scores from local storage or sets to empty array
+    var recordAll = JSON.parse(localStorage.getItem("historyRecord")) || [];
+
+    // sort highscores in decending order
+    recordAll.sort(function (x, y) {
+        return y.score - x.score
+    })
+
+    recordAll.forEach(function (score) {
+        var scoreList = document.createElement("li");
+        scoreList.textContent = score.highScoreInitials + " - " + score.score
+
+    });
+
+    var displayHighScores = document.getElementById("highscore-initials");
+    displayHighScores.appendChild(scoreList);
+
+    // console.log(recordAll);
+    // start.classList.add("hide");
+    // highScoreBtn.classList.add("hide");
+    // highscoreContainer.classList.remove("hide");
+    // highScoreInitials.innerHTML = recordAll.name;
+    // highestScore.innerHTML = recordAll.score;
+    // endSection.classList.add("hide");
+    // start.classList.remove("hide");
+    // start.innerHTML = "Restart";
 };
+
+
+function clearHighScores() {
+    window.localStorage.removeItem("highscoreStorage");
+    window.location.reload();
+};
+
+document.getElementById("clear").onclick = clearHighScores;
+
+showHighScore(); 
