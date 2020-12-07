b40ce4b8d1c32c62719cd2ce045677967b69c9ca
Lu Hao <haolu1125@gmail.com>
2020-12-07 17:26:31 -0500

    moved questions to questions.js

--- quiz.js
+++ quiz.js
@@ -24,58 +24,7 @@ var scoreNum = 0;
 console.log(questions)
 
 
-//question set
-var questions = [
-    {
-        realQ: "How does Harry manage to breathe underwater during the second task of the Triwizard Tournament?",
-        choiceA: "He transfigures into a shark",
-        choiceB: "He kisses a mermaid",
-        choiceC: "He eats gillyweed",
-        choiceD: "He performs a bubble-head charm",
-        correct: "C"
 
-    },
-
-    {
-        realQ: "What is the name of Fred and George’s joke shop?",
-        choiceA: "Weasley Joke Emporium",
-        choiceB: "Weasleys’ Wizard Wheezes",
-        choiceC: "Fred & George’s Wonder Emporium",
-        choiceD: "Zonko’s Joke Shop",
-        correct: "B"
-
-
-    },
-    {
-        realQ: "Which of these is NOT one of the Unforgivable Curses?",
-        choiceA: "Cruciatus Curse",
-        choiceB: "Imperius Curse",
-        choiceC: "Sectumsempra",
-        choiceD: "Avada Kedavra",
-        correct: "C"
-
-    },
-
-    {
-        realQ: "Who played Lord Voldemort in the movies?",
-        choiceA: "Jeremy Irons",
-        choiceB: "Tom Hiddleston",
-        choiceC: "Gary Oldman",
-        choiceD: "Ralph Fiennes",
-        correct: "D"
-
-    },
-
-    {
-        realQ: "Who guards the entrance to the Gryffindor common room?",
-        choiceA: "The Grey Lady",
-        choiceB: "The Fat Friar",
-        choiceC: "The Bloody Baron",
-        choiceD: "The Fat Lady",
-        correct: "D"
-
-    }
-];
 
 
 console.log(questions.length);
@@ -152,8 +101,6 @@ function setTime() {
 
 
 //calculate score
-
-
 
 function rightOrWrong(answer) {
     if (questions[questionIndex].correct == answer && questionIndex !== questions.length - 1) {
